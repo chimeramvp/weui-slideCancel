fc0bfd07bb67ea32442f4674dc11296ef3d49e09
叶川扬 <chimeramvp@qq.com>
2017-04-26 22:36:42 +0800

    优化

--- dist/js/slideCancel.js
+++ dist/js/slideCancel.js
@@ -1,10 +1,10 @@
-function slideCancel(selector, options) {
+function slideCancel(options) {
 
-  var elemArray = document.querySelectorAll(selector || ".weui-cell__cancel");
+  var elemArray = document.querySelectorAll(options.selector || ".weui-cell__cancel");
 
   for (var index = 0; index < elemArray.length; index++) {
     var $this = (function (elem, options) {
-      var startX, startY,  moveWidth;
+      var startX, startY,  moveWidth, moveHeight;
       var elemWidth = elem.offsetWidth;
       var elemHeight = elem.offsetHeight;
       var parentElem = elem.parentNode;
@@ -33,9 +33,9 @@ function slideCancel(selector, options) {
       }
       function move(event) {
         var touch = event.touches[0];
-
+        moveHeight = Math.abs(startY - touch.pageY);
         //有上下移动的时候，判断为用户不是想使用左滑删除功能
-        if(Math.abs(startY - touch.pageY) > 10)
+        if(moveHeight > 10)
           return;
 
         moveWidth = startX - touch.pageX;
@@ -46,7 +46,8 @@ function slideCancel(selector, options) {
         render();
       }
       function end(event) {
-        if (moveWidth < options.mixOffsetWdith() || Math.abs(startY - touch.pageY) > elemHeight) {
+        var touch = event.touches[0];
+        if (moveWidth < options.mixOffsetWdith() || moveHeight > elemHeight) {
           moveWidth = 0;
         }else{
           moveWidth = elemWidth;
